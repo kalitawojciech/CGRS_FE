fe27d9ad8a9c44975348db905a18f8870295563e
Wojciech Kalita <wojtek1311997@gmail.com>
2021-07-22 21:06:27 +0200

    get all games

--- cgrs/src/app/app.module.ts
+++ cgrs/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { GamesTableComponent } from './games-table/games-table.component';
+import { CategoriesService } from './services/categories.service';
+import { GamesService } from './services/games.service';
+import { UserRegistrationComponent } from './user-registration/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesTableComponent,
+    UserRegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CategoriesService,
+    GamesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ cgrs/src/app/games-table/games-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IGame } from '../models/game';
+import { GamesService } from '../services/games.service';
+
+@Component({
+  selector: 'app-games-table',
+  templateUrl: './games-table.component.html',
+  styleUrls: ['./games-table.component.scss']
+})
+export class GamesTableComponent implements OnInit {
+  sub!: Subscription;
+
+  games: IGame[] = [];
+
+  constructor(private gamesService: GamesService) { }
+
+  ngOnInit(): void {
+    this.sub = this.gamesService.getGames().subscribe({
+      next: games => {
+        this.games = games;
+      }
+    })
+  }
+
+
+
+}
